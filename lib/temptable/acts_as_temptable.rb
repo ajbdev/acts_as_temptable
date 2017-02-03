09e38e2b09d0a6094c3187cd144ab2a55ae6ae5d
Andy Baird <andy@threadsculture.com>
2017-02-02 20:30:15 -0600

    Override columns method to look up temp table columns

--- lib/temptable/acts_as_temptable.rb
+++ lib/temptable/acts_as_temptable.rb
@@ -6,13 +6,15 @@ module Temptable
     extend ActiveSupport::Concern
 
     module ClassMethods
-
       def acts_as_temptable(options = {})
         cattr_accessor :temp_table_sql
         cattr_accessor :temp_table_params
+        cattr_accessor :column_cache
         include Temptable::ReadOnlyModel
 
         self.temp_table_sql = options[:sql]
+
+        @columns = {}
       end
 
       def temp_table(params = {})
@@ -54,6 +56,25 @@ module Temptable
         sql = self.temp_table_sql.respond_to?(:call) ? self.temp_table_sql.call(self.temp_table_params) : self.temp_table_sql
 
         connection.execute("CREATE TEMP TABLE #{self.temp_table_name} AS " + sql)
+      end
+
+      def columns
+        @columns[self.temp_table_name] ||= connection.execute( <<SQL
+                  SELECT a.attrelid, a.attname, format_type(a.atttypid, a.atttypmod),
+                         pg_get_expr(d.adbin, d.adrelid), a.attnotnull, a.atttypid, a.atttypmod,
+                         c.collname, col_description(a.attrelid, a.attnum) AS comment
+                    FROM pg_attribute a
+                    LEFT JOIN pg_attrdef d ON a.attrelid = d.adrelid AND a.attnum = d.adnum
+                    LEFT JOIN pg_class cl ON cl.oid = a.attrelid
+                    LEFT JOIN pg_type t ON a.atttypid = t.oid
+                    LEFT JOIN pg_collation c ON a.attcollation = c.oid AND a.attcollation <> t.typcollation
+                   WHERE cl.relkind='r' AND cl.relname = '#{self.temp_table_name}'
+                     AND a.attnum > 0 AND NOT a.attisdropped
+                   ORDER BY a.attnum
+SQL
+        ).to_a.map do |column|
+          ActiveRecord::ConnectionAdapters::PostgreSQLColumn.new(column["attname"], nil, column["format_type"], column["attnotnull"] == 'f')
+        end
       end
     end
   end
